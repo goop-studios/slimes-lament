6370291c1d090498e6496f1b10ca791726e941eb
Nicedinos13 <odin.larsson@gmail.com>
2024-04-05 12:39:22 +0200

    Fixed respawning

--- Slime's Lament/scripts/Respawn.gd
+++ Slime's Lament/scripts/Respawn.gd
@@ -5,3 +5,6 @@ func _on_respawn_pressed():
 
 func _on_return_to_title_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
